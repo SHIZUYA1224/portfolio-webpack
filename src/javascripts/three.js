713889681581be30a9a965f68d99399b47f72e98
SHIZUYA <santaxman@me.com>
2025-11-05 23:11:39 +0900

    test

--- src/javascripts/three.js
+++ src/javascripts/three.js
@@ -6,7 +6,7 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas').appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
+const geometry = new THREE.CircleGeometry();
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
